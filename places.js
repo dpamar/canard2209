f5cf257bfd6077c854674c352c0d627c96c87785
dpamar <dpamar@gmail.com>
2019-09-22 12:54:04 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,3 +1,10 @@
 var places = [
+    [45.180803, 5.72222, 'Juste en face '],
+    [45.181208, 5.723096, 'A droite (et après à gauche)'],
+    [45.181253, 5.722871, 'A gauche '],
+    [45.181695, 5.722702, 'Trottoir en face'],
+    [45.182111, 5.722713, 'Interdit pour les chiens pas pour les enfants !'],
+    [45.183129, 5.722733, 'Il est complet...'],
+    [45.183192, 5.722927, 'Dans le même coin-coin ']
 ];
-
+//Resume game designer URL: https://dpamar.github.io/anniv_shana/generator.html?places=W1s0NS4xODA4MDMsIDUuNzIyMjIwLCAnSnVzdGUgZW4gZmFjZSAnXSxbNDUuMTgxMjA4LCA1LjcyMzA5NiwgJ0EgZHJvaXRlIChldCBhcHLocyDgIGdhdWNoZSknXSxbNDUuMTgxMjUzLCA1LjcyMjg3MSwgJ0EgZ2F1Y2hlICddLFs0NS4xODE2OTUsIDUuNzIyNzAyLCAnVHJvdHRvaXIgZW4gZmFjZSddLFs0NS4xODIxMTEsIDUuNzIyNzEzLCAnSW50ZXJkaXQgcG91ciBsZXMgY2hpZW5zIHBhcyBwb3VyIGxlcyBlbmZhbnRzICEnXSxbNDUuMTgzMTI5LCA1LjcyMjczMywgJ0lsIGVzdCBjb21wbGV0Li4uJ10sWzQ1LjE4MzE5MiwgNS43MjI5MjcsICdEYW5zIGxlIG3qbWUgY29pbi1jb2luICddXQ==
